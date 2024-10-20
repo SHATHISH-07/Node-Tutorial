9edec121529a7595608f2bafd0fce5eafe3e92e8
SHATHISH-07 <satheesh12072004@gmail.com>
2024-10-20 10:08:17 +0530

    Added some file

--- app.js
+++ app.js
@@ -1,7 +1,39 @@
-const _ = require("lodash");
+const { readFile, writeFile } = require("fs").promises;
 
-const items = [1, [2, [3, [4]]]];
+// const util = require("util");
 
-const newItems = _.flattenDeep(items);
-console.log("✌️newItems --->", newItems);
-console.log("-hello there");
+// const readFilePromise = util.promisify(readFile);
+// const writeFilePromise = util.promisify(writeFile);
+
+const start = async () => {
+  try {
+    const first = await readFile("./content/first.txt", "utf8");
+    const second = await readFile("./content/second.txt", "utf8");
+    await writeFile(
+      "./content/result-async-promisified.txt",
+      `${first}\n${second}`,
+      "utf8"
+    );
+    console.log(`THIS IS AWESOME : ${first} ${second}`);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+start();
+
+// const getText = (path) => {
+//   return new Promise((resolve, reject) => {
+//     readFile(path, "utf-8", (err, data) => {
+//       if (err) {
+//         reject(err);
+//       } else {
+//         resolve(data);
+//       }
+//     });
+//   });
+// };
+
+// getText("./content/first.txt")
+//   .then((result) => console.log(result))
+//   .catch((err) => console.log(err));
